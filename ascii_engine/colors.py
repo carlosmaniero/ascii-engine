accc09b8530e60f5f1e2c6323cea19aa883c97f3
Carlos Maniero <carlosmaniero@gmail.com>
2018-03-11 17:56:31 -0300

    Improve performance by add a little cache system in the color render

--- ascii_engine/colors.py
+++ ascii_engine/colors.py
@@ -2,8 +2,14 @@ from collections import namedtuple
 from ascii_engine.term_colors import TERM_COLOR_MAP
 
 
+cached_colors = {}
+
+
 class RGB(namedtuple('RGBBase', ['r', 'g', 'b'])):
     def get_term_color(self):
+        if self in cached_colors:
+            return cached_colors[self]
+
         diff = 255 ** 3
         best_choice = 0
 
@@ -18,4 +24,5 @@ class RGB(namedtuple('RGBBase', ['r', 'g', 'b'])):
                 best_choice = term_color
                 diff = current_diff
 
+        cached_colors[self] = best_choice
         return best_choice

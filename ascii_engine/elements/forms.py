6a88a75999001701107e7ee306b50d19014bc3b3
Carlos Maniero <carlosmaniero@gmail.com>
2018-04-10 23:27:23 -0300

    #1 Add placeholder style to input field

--- ascii_engine/elements/forms.py
+++ ascii_engine/elements/forms.py
@@ -11,15 +11,25 @@ class InputField(Text):
         super().__init__(state.value)
         self.state = state
         self.__focus_style = []
+        self.__placeholder_style = []
 
     def set_focus_style(self, styles):
         self.__focus_style = styles
+
+    def set_placeholder_style(self, styles):
+        self.__placeholder_style = styles
 
     def to_pixels(self):
         if self.state.is_focused and self.__focus_style:
             return functools.reduce(
                 lambda current, style: style(current),
                 self.__focus_style,
+                self.create_fragment()
+            )
+        if self.state.value == '' and self.__placeholder_style:
+            return functools.reduce(
+                lambda current, style: style(current),
+                self.__placeholder_style,
                 self.create_fragment()
             )
         return super().to_pixels()
--- tests/elements/test_form.py
+++ tests/elements/test_form.py
@@ -56,8 +56,71 @@ def test_that_it_is_stylised_with_the_focused_style_when_not_focused():
     stubed_styles[2].assert_called_once_with(stubed_styles[1].return_value)
 
 
+def test_that_it_is_stylised_with_the_focused_style_even_when_not_filled():
+    given_text = ''
+    input_field = InputField(
+        InputField.State(given_text, True, 0)
+    )
+
+    stubed_styles = [
+        MagicMock(),
+        MagicMock(),
+        MagicMock(),
+    ]
+
+    input_field.set_focus_style(stubed_styles)
+    fragment = input_field.to_pixels()
+
+    assert fragment == stubed_styles[2].return_value
+    assert stubed_styles[0].called
+    stubed_styles[1].assert_called_once_with(stubed_styles[0].return_value)
+    stubed_styles[2].assert_called_once_with(stubed_styles[1].return_value)
+
+
 def test_that_it_is_stylised_with_the_normal_style_when_no_focus_style():
     given_text = 'Hello, World!'
+    input_field = InputField(
+        InputField.State(given_text, True, 0)
+    )
+
+    stubed_styles = [
+        MagicMock(),
+        MagicMock(),
+        MagicMock(),
+    ]
+
+    input_field.set_style(stubed_styles)
+    fragment = input_field.to_pixels()
+
+    assert fragment == stubed_styles[2].return_value
+    assert stubed_styles[0].called
+    stubed_styles[1].assert_called_once_with(stubed_styles[0].return_value)
+    stubed_styles[2].assert_called_once_with(stubed_styles[1].return_value)
+
+
+def test_that_it_is_stylised_with_the_placeholder_style_when_not_filled():
+    given_text = ''
+    input_field = InputField(
+        InputField.State(given_text, True, 0)
+    )
+
+    stubed_styles = [
+        MagicMock(),
+        MagicMock(),
+        MagicMock(),
+    ]
+
+    input_field.set_placeholder_style(stubed_styles)
+    fragment = input_field.to_pixels()
+
+    assert fragment == stubed_styles[2].return_value
+    assert stubed_styles[0].called
+    stubed_styles[1].assert_called_once_with(stubed_styles[0].return_value)
+    stubed_styles[2].assert_called_once_with(stubed_styles[1].return_value)
+
+
+def test_that_it_is_stylised_with_the_normal_style_when_placeholder_not_set():
+    given_text = ''
     input_field = InputField(
         InputField.State(given_text, True, 0)
     )
